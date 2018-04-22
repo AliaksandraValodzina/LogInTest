e5c2d65701386474ca5a10a26bb1bb241c5a37eb
AliaksandraValodzina <shoora.volodina@gmail.com>
2018-04-22 20:24:31 +0300

    test

---
+++ LogIn/PageObject.cs
@@ -0,0 +1,63 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Support.UI;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LogIn
+{
+    public class PageObject
+    {
+        private IWebDriver driver;
+
+        #region Selectors
+
+        By userNameBy = By.CssSelector("input[type = 'email']");
+        By nextButtonBy = By.CssSelector("div[role = 'button']");
+        By passwordBy = By.CssSelector("input[type = 'password']");
+        By tableBy = By.Id("div[role = 'tabpanel']/tbody']");
+        #endregion
+
+        public PageObject(IWebDriver driver)
+        {
+            this.driver = driver;
+        }
+
+        public PageObject SetUserNameAndPassword(string userName, string password)
+        {
+            driver.FindElement(userNameBy).Clear();
+            driver.FindElement(userNameBy).SendKeys(userName);
+            driver.FindElement(nextButtonBy).Click();
+
+            this.WaitUntilElementExists(passwordBy);
+            driver.FindElement(passwordBy).Clear();
+            driver.FindElement(passwordBy).SendKeys(password);
+            driver.FindElement(nextButtonBy).Click();
+
+            this.WaitUntilElementExists(tableBy);
+
+            return this;
+        }
+
+        public bool IsTableVisible()
+        {
+            return driver.FindElement(tableBy).Displayed;
+        }
+
+        public IWebElement WaitUntilElementExists(By elementLocator, int timeout = 10)
+        {
+            try
+            {
+                var wait = new WebDriverWait(driver, TimeSpan.FromSeconds(timeout));
+                return wait.Until(ExpectedConditions.ElementExists(elementLocator));
+            }
+            catch (NoSuchElementException)
+            {
+                Console.WriteLine("Element with locator: '" + elementLocator + "' was not found in current context page.");
+                throw;
+            }
+        }
+    }
+}
---
+++ LogIn/UnitTest1.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+
+
+namespace LogIn
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        private PageObject pageObject;
+        private IWebDriver driver;
+        private ChromeOptions options;
+
+        private string baceUrl = "https://www.gmail.com";
+
+        [TestInitialize]
+        public void TestInitialize()
+        {
+            options = new ChromeOptions();
+            options.AddArgument(@"C:\Users\Volodin\Documents\Visual Studio 2015\Projects\LogIn\LogIn\DriverOptions");
+
+            driver = new ChromeDriver(options);
+            pageObject = new PageObject(driver);
+            driver.Navigate().GoToUrl(baceUrl);        }
+
+
+        [TestMethod]
+        public void LogInTest()
+        {
+            pageObject.SetUserNameAndPassword("shura.volodina@gmail.com", "130419901512");
+            Assert.IsTrue(pageObject.IsTableVisible(), "Check that table appears on the page");
+        }
+
+        [TestCleanup]
+        public void TestCleanup()
+        {
+            driver.Quit();
+        }
+    }
+}
